b8c85cb48abf08025073cf912ac0fdd65873a3bc
Daniel Gomes <danielgjrosa@gmail.com>
2025-04-14 20:47:36 -0300

    Delete NaturaNexus/backend directory

--- NaturaNexus/backend/models/contato.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from backend.database import Base
-
-class Contato(Base):
-    __tablename__ = "contatos"
-
-    id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String, index=True)
-    mensagem = Column(String)
